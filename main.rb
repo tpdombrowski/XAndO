2f3ae6dda957a42de2ede0b3dba61ff90881992d
Tom Dombrowski <thomas.dombrowski@taxware.com>
2013-12-10 14:33:14 -0500

    I fixed the folder structure.  It was my way of avoiding real work.

--- main.rb
+++ main.rb
@@ -1,7 +1,16 @@
 #!/usr/bin/env ruby
-require 'rubygems'
-require 'gosu'
-require 'chingu'
+require 'rubygems' rescue nil
+begin
+  raise LoadError if defined?(Ocra)
+  require '../chingu/lib/chingu'
+rescue LoadError
+  require 'chingu'
+end
+include Gosu
+include Chingu
+
+require_rel 'src/*'
+DEBUG = false
 
 class Game < Chingu::Window
 	BACKGROUND_COLOR = Gosu::Color.rgba(200, 100, 100, 200)
@@ -9,11 +18,9 @@ class Game < Chingu::Window
 	WINDOW_HEIGHT = 640
 	
 	def initialize
-		
 		super(WINDOW_HEIGHT,WINDOW_WIDTH,false)
 		self.caption = "My Window"
 		@player1 = Player.create(:x => 35, :y => 35)
-		
 	end
 	
 	def setup
@@ -34,57 +41,11 @@ class Game < Chingu::Window
 		0)
 	end
 	
-end
-
-class Player < Chingu::GameObject
-	
-	
-	def initialize(options = {})
-		options = {
-			x: 0,
-			y: 0,
-			image: 'RED.png'
-		}.merge! options
-		
-		super options
-	end
-	
-	def setup
-		self.input = {  [:holding_left, :holding_a] => :holding_left, 
-                    [:holding_right, :holding_d] => :holding_right, 
-					[:holding_down,  :holding_s] => :holding_down, 
-					[:holding_up,    :holding_w] => :holding_up, }
-    
-		self.zorder = 1
-		@speed = 4
-   
-	end
-	
-	def holding_left
-		move(-@speed, 0)
-	end
-	
-	def holding_right
-		move(@speed, 0)
-	end
-	
-	def holding_up
-		move(0, -@speed)
-	end
-	
-	def holding_down
-		move(0, @speed)
-	end
-	
-	def move(x,y)
-		self.factor_x = self.factor_x.abs   if x > 0
-		self.factor_x = -self.factor_x.abs  if x < 0
-    
-		self.x += x
-    
-		self.y += y
+	def self.run
+		new.show
 	end
 	
 end
 
-Game.new.show
+Game.run
+
---
+++ src/player.rb
@@ -0,0 +1,49 @@
+class Player < Chingu::GameObject
+	
+	def initialize(options = {})
+		options = {
+			x: 0,
+			y: 0,
+			image: 'spacecraft.png'
+		}.merge! options
+		
+		super options
+	end
+	
+	def setup
+		self.input = {  [:holding_left, :holding_a] => :holding_left, 
+                    [:holding_right, :holding_d] => :holding_right, 
+					[:holding_down,  :holding_s] => :holding_down, 
+					[:holding_up,    :holding_w] => :holding_up, }
+    
+		self.zorder = 1
+		@speed = 4
+   
+	end
+	
+	def holding_left
+		move(-@speed, 0)
+	end
+	
+	def holding_right
+		move(@speed, 0)
+	end
+	
+	def holding_up
+		move(0, -@speed)
+	end
+	
+	def holding_down
+		move(0, @speed)
+	end
+	
+	def move(x,y)
+		self.factor_x = self.factor_x.abs   if x > 0
+		self.factor_x = -self.factor_x.abs  if x < 0
+    
+		self.x += x
+    
+		self.y += y
+	end
+	
+end
